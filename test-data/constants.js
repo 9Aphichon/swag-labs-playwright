a7a8371c4c5f5137dda1af36ef2b83710966926a
Aphichon <9aphichon@gmail.com>
2023-07-20 22:26:55 +0700

    create test data

---
+++ test-data/constants.js
@@ -0,0 +1,11 @@
+export const ERROR_MESSAGES = {
+    invalidCredentials: 'Epic sadface: Username and password do not match any user in this service',
+    usernameIsRequired: 'Epic sadface: Username is required',
+    passwordIsRequired: 'Epic sadface: Password is required',
+    userLockedOut: 'Epic sadface: Sorry, this user has been locked out.'
+};
+  
+export const URL = {
+    loginPage: 'https://qa-challenge.codesubmit.io/',
+    inventoryPage: 'https://qa-challenge.codesubmit.io/inventory.html'
+};
---
+++ test-data/user-accounts.js
@@ -0,0 +1,5 @@
+export const standard_user = { username: 'standard_user', password: 'secret_sauce' };
+export const invalid_user = { username: 'invalid_user', password: 'zxcvbn'};
+export const locked_out_user = { username: 'locked_out_user', password: 'secret_sauce' };
+export const problem_user = { username: 'problem_user', password: 'secret_sauce' };
+export const performance_glitch_user = { username: 'performance_glitch_user', password: 'secret_sauce' };
